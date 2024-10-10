b8db2cddf5b9f5df00dab4226fadf21bd7074fae
Taewon Yang <for.code.won@gmail.com>
2024-10-10 19:14:17 +0900

    Update lyricsData.ts

--- app/lyricsData.ts
+++ app/lyricsData.ts
@@ -11,69 +11,69 @@ export const playlistData: Record<string, PlaylistData> = {
       {
         title: "*Beautiful",
         artist: "Wanna One (워너원)",
-        group: "2017",
+        group: "2017년",
         youtubeId: "xXWyJEo6VgA",
         lyrics: "[직접 찾아 들으면서 가사를 아직까지\n잘 기억하고 있는 첫번째 곡]"
       },
       {
         title: "Yes or Yes",
         artist: "TWICE(트와이스)",
-        group: "2018",
+        group: "2018년",
         youtubeId: "mAKsZ26SabQ",
         lyrics: year_lyrics
       },
       {
         title: "사랑을 했다",
         artist: "iKON",
-        group: "2018",
+        group: "2018년",
         youtubeId: "vecSVX1QYbQ",
         lyrics: year_lyrics
       },
       {
         title: "Way Back Home",
         artist: "숀 (SHAUN)",
-        group: "2018",
+        group: "2018년",
         youtubeId: "amOSaNX7KJg",
         lyrics: year_lyrics
       },
       {
         title: "*옥탑방",
         artist: "N.Flying(엔플라잉)",
-        group: "2019",
+        group: "2019년",
         youtubeId: "VpaUh_BGqE0",
         lyrics: "[듣자마자 5학년 생각나는 곡]"
       },
       {
         title: "*작은 것들을 위한 시",
         artist: "방탄소년단(BTS)",
-        group: "2019",
+        group: "2019년",
         youtubeId: "oFuY5M1lwiY",
         lyrics: "[방탄 입덕하고 나서 처음으로 발표된 곡]"
       },
       {
         title: "Dynamite",
         artist: "BTS(방탄소년단)",
-        group: "2020",
+        group: "2020년",
         youtubeId: "gdZLi9oWNZg",
         lyrics: year_lyrics
       },
       {
         title: "Any song(아무노래)",
         artist: "ZICO(지코)",
-        group: "2020",
+        group: "2020년",
         youtubeId: "UuV2BmJ1p_I",
         lyrics: year_lyrics
       },
       {
         title: "*Life Goes On",
         artist: "BTS (방탄소년단)",
-        group: "2020",
+        group: "2020년",
         youtubeId: "-5q5mZbe3V8",
         lyrics: "끝이 보이지 않아\n출구가 있긴 할까\n발이 떼지질 않아 않아 oh\n잠시 두 눈을 감아\n여기 내 손을 잡아\n저 미래로 달아나자\nLike an echo in the forest\n하루가 돌아오겠지\n아무 일도 없단 듯이\nYeah life goes on\nLike an arrow in the blue sky\n또 하루 더 날아가지\nOn my pillow, on my table\nYeah life goes on\nLike this again"
       }
     ]
   },
-  "2021-2023": {
+  "2021-2022": {
     tracks: [
       {
         title: "Celebrity",
@@ -188,11 +188,79 @@ export const playlistData: Record<string, PlaylistData> = {
         lyrics: year_lyrics
       },
       {
+        title: "Nxde",
+        artist: "(여자)아이들((G)I-DLE)",
+        group: "2022년",
+        youtubeId: "fCO7f0SmrDc",
+        lyrics: year_lyrics
+      },
+      {
+        title: "ANTIFRAGILE",
+        artist: "LE SSERAFIM(르세라핌)",
+        group: "2022년",
+        youtubeId: "yLupcG_eFag",
+        lyrics: year_lyrics
+      },
+      {
+        title: "Candy",
+        artist: "NCT DREAM(엔시티 드림)",
+        group: "2022년",
+        youtubeId: "7avTpOILp1U",
+        lyrics: year_lyrics
+      },
+      {
         title: "*Numb Little Bug",
         artist: "Em Beihold",
         group: "2022년 +",
         youtubeId: "FbpzrymWpT8",
         lyrics: "Do you ever get a little bit tired of life?​\n너도 사는 게 조금 지겨워질 때가 있어?​\nLike you're not really happy but you don't wanna die​​\n왜 막 행복한 것도 아닌데 죽고 싶은 것도 아니고​\nLike you're hangin' by a thread but you gotta survive​​\n썩은 동앗줄에 겨우 매달린 것 같은데 꼭 살아남아야 하는 느낌​\n'Cause you gotta survive​​\n견뎌내야만 하니까​\nLike your body's in the room but you're not really there​​\n몸은 이 방 안에 있는데한데 정신은 거기 없고​\nLike you have empathy inside but you don't really care​​\n다른 사람이 힘들면 안타깝긴 한데 딱히 챙겨주는 건 없어​\nLike you're fresh outta love but it's been in the air​​\n사랑은 다 식었는데 그냥 붙어만 있는 것처럼​\nAm I past repair?​​\n이제 난 고쳐지기도 힘든 걸까?"
+      },
+      
+    ]
+  },
+  "2023~2024": {
+    tracks: [
+      {
+        title: "*Ditto",
+        artist: "NewJeans(뉴진스)",
+        group: "2023년 1월",
+        youtubeId: "Km71Rr9K-Bw",
+        lyrics: "[말레이시아 여행 가서 이 노래 자주 들었더니\n디토 들을때마다 여행 가고 싶어짐]"
+      },
+      {
+        title: "*VIBE (feat. Jimin of BTS)",
+        artist: "TAEYANG(태양)",
+        group: "2023년 1월",
+        youtubeId: "bgnk0-0Y0Gk",
+        lyrics: "[Ditto랑 같은 이유]"
+      },
+      {
+        title: "*Take Two",
+        artist: "BTS(방탄소년단)",
+        group: "2023년",
+        youtubeId: "owjVpYCmwcg",
+        lyrics: "[방탄 노래는 힘들수록 더 많이 듣게 되는듯\n25년 컴백을 기다리며 10주년]\n\nCan't you see the take two\nLetters I didn't send to you\n너와 함께하는 청춘\n시작일지도 몰라\nYeah we never felt so young\nWhen together sing the song\n서로의 눈 맞추며\n어둠 속에도"
+      },
+      {
+        title: "*Left Right Left",
+        artist: "Charlie Puth",
+        group: "2023년 +",
+        youtubeId: "owjVpYCmwcg",
+        lyrics: "Ohh, I been hurtin' lately\n요즘 난 아파\nI'm down on all my luck\n운이 너무 안 좋네\nSometimes there's no one there by your side\n가끔은 옆에 아무도 없을 때가 있어\nI don't deserve this baby\n이런 건 나한테 어울리지 않지만\nBut I know that I can't give up\n포기할 수 없단 건 알아\nTough times I just take them in stride\n힘든 시기는 그냥 참고 이겨내\nHmmm, I'm almost there, only one more step\n흠, 거의 다 왔어, 한 발짝만 더 가면 돼\n(Left, right, left, right, left)\n(왼발, 오른발, 왼발, 오른발, 왼발)\n...\nMmm I know it looks like it's too far\n음, 너무 멀어 보이겠지만\nBut it's nothing compared to how far we've come\n우리가 온 거리에 비하면 아무것도 아니야"
+      },
+      {
+        title: "*Changes",
+        artist: "Lauv",
+        group: "2023년 +",
+        youtubeId: "UMW9NmlX1ws",
+        lyrics: "I'm getting rid of my clothes I don’t wear\n​안 입는 옷들을 정리하고 있어\nI think I'm gonna cut my hair\n머리도 짧게 자를까봐\nCause these days I don’t feel like me\n요즘 내가 나같지가 않거든\nProbably won't last that long\n아마 오래 가진 못할 거야\nBut Lord knows I could use some sleep\n한숨 자고 일어나야 될 것 같아 \nChanges, they might drive you half-insane\n변화들은 널 반쯤 미치게 할 수도 있지만\nBut it's killing you to stay the same\n머물러 있는 것보단 그게 나을거야\nBut it's all gonna work out\n모든게 다 잘 풀릴거야\nIt's all gonna work out someday\n결국엔 다 잘 될 거야 \nMoments, livin' with your eyes half-open\n눈을 반쯤뜨고 살아온 순간들 속에서\nYou've been thinking bout these changes\n계속해서 변화하고 싶어왔겠지\nIt's all gonna work out\n모든게 다 잘 풀릴거야\nIt's all gonna work out someday\n결국엔 다 잘 될 거야 \nI think I’m gonna take some pills to fix my brain​\n머리가 복잡해서 약을 먹어볼까봐\nCause I tried every other way\n다른 방법들도 많이 써봤지만\nBut some things you can't fix yourself\n나 스스로도 못 고치는게 있더라\nBut he said that he's doing the same thing over and over\n하지만 사람들은 같은 일을 반복할뿐이야\nAnd life ain’t easy these days\n요즘 사는게 쉽지 않아\nNo, life ain't easy these days\n​정말 쉽지 않더라"
+      },
+      {
+        title: "*Nightmares",
+        artist: "Easy Life",
+        group: "2023년 +",
+        youtubeId: "N6Z3yRf0C9o",
+        lyrics: "[Ditto랑 같은 이유]"
       },
     ]
   },
@@ -202,15 +270,15 @@ export const playlistData: Record<string, PlaylistData> = {
         title: "소우주",
         artist: "BTS(방탄소년단)",
         group: "가사를 좋아하는 곡들",
-        youtubeId: "LXOJk2PFKgY",
-        lyrics: "[밤이 깊을수록 더 빛나는 별빛]\n우린 빛나고 있네\n각자의 방 각자의 별에서\n어떤 빛은 야망\n어떤 빛은 방황\n사람들의 불빛들\n모두 소중한 하나\n어두운 밤 (외로워 마)\n별처럼 다 (우린 빛나)\n사라지지 마\n큰 존재니까\nLet us shine\n...\n칠흑 같던 밤들 속에\nShine, dream, smile\nOh let us light up the night\n우린 우리대로 빛나\nShine, dream, smile\nOh let us light up the night\n우리 그 자체로 빛나\nTonight"
+        youtubeId: "Iq6RdCTLBd8",
+        lyrics: "[밤이 깊을수록 더 빛나는 별빛]\n\n우린 빛나고 있네\n각자의 방 각자의 별에서\n어떤 빛은 야망\n어떤 빛은 방황\n사람들의 불빛들\n모두 소중한 하나\n어두운 밤 (외로워 마)\n별처럼 다 (우린 빛나)\n사라지지 마\n큰 존재니까\nLet us shine\n...\n칠흑 같던 밤들 속에\nShine, dream, smile\nOh let us light up the night\n우린 우리대로 빛나\nShine, dream, smile\nOh let us light up the night\n우리 그 자체로 빛나\nTonight"
       },
       {
         title: "Answer : Love Myself",
         artist: "BTS(방탄소년단)",
         group: "가사를 좋아하는 곡들",
         youtubeId: "3POhEmU0llA",
-        lyrics: "눈을 뜬다 어둠 속 나\n심장이 뛰는 소리 낯설 때\n마주 본다 거울 속 너\n겁먹은 눈빛 해묵은 질문\n어쩌면 누군가를 사랑하는 것보다\n더 어려운 게 나 자신을 사랑하는 거야\n솔직히 인정할 건 인정하자\n니가 내린 잣대들은 너에게 더 엄격하단 걸\n니 삶 속의 굵은 나이테\n그 또한 너의 일부, 너이기에\n이제는 나 자신을 용서하자 버리기엔\n우리 인생은 길어 미로 속에선 날 믿어\n겨울이 지나면 다시 봄은 오는 거야\n차가운 밤의 시선\n초라한 날 감추려\n몹시 뒤척였지만\n저 수많은 별을 맞기 위해 난 떨어졌던가\n저 수천 개 찬란한 화살의 과녁은 나 하나\nYou've shown me I have reasons\nI should love myself\n내 숨 내 걸어온 길 전부로 답해\n어제의 나 오늘의 나 내일의 나\n(I'm learning how to love myself)\n빠짐없이 남김없이 모두 다 나\n정답은 없을지도 몰라\n어쩜 이것도 답은 아닌 거야\n그저 날 사랑하는 일조차\n누구의 허락이 필요했던 거야\n난 지금도 나를 또 찾고 있어\nBut 더는 죽고 싶지가 않은 걸\n슬프던 me\n아프던 me\n더 아름다울 美\n그래 그 아름다움이\n있다고, 아는 마음이\n나의 사랑으로 가는 길\n가장 필요한 나다운 일\n지금 날 위한 행보는\n바로 날 위한 행동\n날 위한 태도\n그게 날 위한 행복\nI'll show you what i got\n두렵진 않아 그건 내 존재니까\nLove myself\n시작의 처음부터\n끝의 마지막까지\n해답은 오직 하나\n왜 자꾸만 감추려고만 해 니 가면 속으로\n내 실수로 생긴 흉터까지 다 내 별자린데\nYou've shown me I have reasons\nI should love myself\n내 숨 내 걸어온 길 전부로 답해\n내 안에는 여전히\n서툰 내가 있지만\nYou've shown me I have reasons\nI should love myself\n내 숨 내 걸어온 길 전부로 답해\n어제의 나 오늘의 나 내일의 나\n(I'm learning how to love myself)\n빠짐없이 남김없이 모두 다 나"
+        lyrics: "[어쩌면 누군가를 사랑하는 것보다\n더 어려운 게 나 자신을 사랑하는 거야]\n\n눈을 뜬다 어둠 속 나\n심장이 뛰는 소리 낯설 때\n마주 본다 거울 속 너\n겁먹은 눈빛 해묵은 질문\n어쩌면 누군가를 사랑하는 것보다\n더 어려운 게 나 자신을 사랑하는 거야\n솔직히 인정할 건 인정하자\n니가 내린 잣대들은 너에게 더 엄격하단 걸\n니 삶 속의 굵은 나이테\n그 또한 너의 일부, 너이기에\n이제는 나 자신을 용서하자 버리기엔\n우리 인생은 길어 미로 속에선 날 믿어\n겨울이 지나면 다시 봄은 오는 거야\n차가운 밤의 시선\n초라한 날 감추려\n몹시 뒤척였지만\n저 수많은 별을 맞기 위해 난 떨어졌던가\n저 수천 개 찬란한 화살의 과녁은 나 하나\nYou've shown me I have reasons\nI should love myself\n내 숨 내 걸어온 길 전부로 답해\n어제의 나 오늘의 나 내일의 나\n(I'm learning how to love myself)\n빠짐없이 남김없이 모두 다 나\n정답은 없을지도 몰라\n어쩜 이것도 답은 아닌 거야\n그저 날 사랑하는 일조차\n누구의 허락이 필요했던 거야\n난 지금도 나를 또 찾고 있어\nBut 더는 죽고 싶지가 않은 걸\n슬프던 me\n아프던 me\n더 아름다울 美\n그래 그 아름다움이\n있다고, 아는 마음이\n나의 사랑으로 가는 길\n가장 필요한 나다운 일\n지금 날 위한 행보는\n바로 날 위한 행동\n날 위한 태도\n그게 날 위한 행복\nI'll show you what i got\n두렵진 않아 그건 내 존재니까\nLove myself\n시작의 처음부터\n끝의 마지막까지\n해답은 오직 하나\n왜 자꾸만 감추려고만 해 니 가면 속으로\n내 실수로 생긴 흉터까지 다 내 별자린데\nYou've shown me I have reasons\nI should love myself\n내 숨 내 걸어온 길 전부로 답해\n내 안에는 여전히\n서툰 내가 있지만\nYou've shown me I have reasons\nI should love myself\n내 숨 내 걸어온 길 전부로 답해\n어제의 나 오늘의 나 내일의 나\n(I'm learning how to love myself)\n빠짐없이 남김없이 모두 다 나"
       },
       {
         title: "Whalien 52",
@@ -224,7 +292,7 @@ export const playlistData: Record<string, PlaylistData> = {
         artist: "j-hope",
         group: "가사를 좋아하는 곡들",
         youtubeId: "OK3GJ0WIQ8s",
-        lyrics: "내 성격은 half and half who knows?\n공인으로서의 노력하는 life who knows?\n큰 욕구는 자제하는 중 who knows?\n늘 공상에 차오르는 숨 who knows?\n편히 울고 싶은 거 I know\n미친 듯 놀고 싶은 거 I know\n사랑에 설레고 싶은 거 I know\n...\n한 번쯤 다른 나의 인생 그림\n그려 보고 싶은 거야, 도화지 dream\nSleep\n저기 저 경계선을 넘어\n느껴보는 거야\n앨리스가 빠진 구멍처럼\n호그와트 가는 길처럼\n신기루의 세계일 거야\n그리고 내 세상일 거야\n상상 그대로 일 거야\nBut 영원하진 않을 거야\nShit",
+        lyrics: "큰 욕구는 자제하는 중 who knows?\n늘 공상에 차오르는 숨 who knows?\n편히 울고 싶은 거 I know\n미친 듯 놀고 싶은 거 I know\n사랑에 설레고 싶은 거 I know\n...\n한 번쯤 다른 나의 인생 그림\n그려 보고 싶은 거야, 도화지 dream\nSleep\n저기 저 경계선을 넘어\n느껴보는 거야\n앨리스가 빠진 구멍처럼\n호그와트 가는 길처럼\n신기루의 세계일 거야\n그리고 내 세상일 거야\n상상 그대로 일 거야\nBut 영원하진 않을 거야\nShit",
       }
     ]
   },
@@ -310,6 +378,20 @@ export const playlistData: Record<string, PlaylistData> = {
         group: "아포방포(BTS)",
         youtubeId: "02ngFtfAdas",
         lyrics: "[제일 좋아하는 무대]"
+      },
+      {
+        title: "MAMA2017 Micdrop",
+        artist: "BTS(방탄소년단)",
+        group: "아포방포(BTS)",
+        youtubeId: "nfHuViQfdho",
+        lyrics: "'no lyrics'"
+      },
+      {
+        title: "MAMA2014 GOOD BOY & FANTASTIC BABY",
+        artist: "BIGBANG(빅뱅)",
+        group: "BIGBANG(빅뱅)",
+        youtubeId: "-UZXEFzWXfc",
+        lyrics: "'no lyrics'"
       }
     ]
   }
